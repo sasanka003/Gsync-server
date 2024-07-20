d199f9097eb890044dec7a6cbf956f11ad5a04fb
HansakaHeli <programminghhr66@gmail.com>
2024-07-20 14:47:10 +0530

    Create DbPost model

--- server/database/models.py
+++ server/database/models.py
@@ -10,3 +10,14 @@ class DbUser(Base):
     username = Column(String)
     email = Column(String, unique=True)
     password = Column(String)
+    posts = relationship('DbPost', back_populates='user')
+
+class DbPost(Base):
+    __tablename__ = "post"
+    postid = Column(String, primary_key=True)
+    title = Column(String)
+    description = Column(String)
+    image = Column(String)
+    dateshared = Column()
+    userid = Column(String, ForeignKey('users.userId'))
+    user = relationship('DbUser', back_populates='posts')
