8e6408ddf9188b73dc9935a7cf6b29a72430f566
HansakaHeli <programminghhr66@gmail.com>
2024-07-21 12:54:01 +0530

    Creaet get request for get all post

--- server/router/post.py
+++ server/router/post.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import Session
 from database import db_post
 from database.database import get_db
 from schemas.post import PostBase, PostDisplay
+from typing import List
+
 
 router = APIRouter(
     prefix='/post',
@@ -13,3 +15,7 @@ router = APIRouter(
 @router.post('',response_model=PostDisplay)
 def create_post(request: PostBase, db: Session = Depends(get_db)):
     return db_post.create(db,request)
+
+@router.get("/all",response_model=List[PostDisplay])
+def get_all_posts(db: Session = Depends(get_db)):
+    return db_post.get_all(db)
