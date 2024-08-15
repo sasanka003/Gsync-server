ba23387e1aa7a50d110d58a3241c1c2fe0af27ef
HansakaHeli <programminghhr66@gmail.com>
2024-08-16 00:49:55 +0530

    Create update_plantation_status endpoint

--- server/schemas/admin.py
+++ server/schemas/admin.py
@@ -5,6 +5,14 @@ class GradenersDisplay(BaseModel):
     user_id: uuid.UUID
     name: str
     email: str
-    # phone: str
+    phone: str
     class Config:
         from_attributes = True
+
+class PlantationDisplay(BaseModel):
+    plantation_id: int
+    type:str
+    user_id: uuid.UUID
+    city: str # address
+    createdAt:str
+    status: str
