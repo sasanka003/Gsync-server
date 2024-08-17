6cdaf588ca25356090eaabebf454e0cc306fc0d1
visith1577 <visithkumarapperuma@gmail.com>
2024-08-17 15:18:47 +0530

    contact us mail service

---
+++ server/services/mail_service.py
@@ -0,0 +1,21 @@
+from fastapi_mail import FastMail, ConnectionConfig
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MAIL_TO = os.getenv("MAIL_TO")
+
+conf = ConnectionConfig(
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME"),
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD"),
+    MAIL_FROM = os.getenv("MAIL_FROM"),
+    MAIL_PORT = 2525,
+    MAIL_SERVER = "mail.smtp2go.com",
+    MAIL_SSL_TLS=False,
+    MAIL_STARTTLS=False,
+    USE_CREDENTIALS = True,
+    TEMPLATE_FOLDER="server/templates"
+)
+
+fm = FastMail(conf)
