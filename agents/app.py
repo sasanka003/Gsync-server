51e2491a6529ec3e018a1d33cc4c2693d0159997
visith kumarapperuma <visithr9@gmail.com>
2024-12-03 16:17:46 +0530

    backend endpoint fixed

--- agents/app.py
+++ agents/app.py
@@ -9,6 +9,7 @@ from agents.core import research_agent
 from crew import parse_agricultural_data
 from crews.enterprise_crew.enterprise_crew import EnterpriseAnalystCrew
 from crews.iot_crew.iot_crew import IotAnalystCrew
+from pydantic import BaseModel
 from services import fm
 
 logfire.configure()
@@ -19,6 +20,22 @@ app = FastAPI(
     version="0.1"
 )
 logfire.instrument_fastapi(app)
+
+origins = [
+    'http://localhost:3000'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+class JsonOutput(BaseModel):
+    output: str
+
 
 def generate_plant_data():
     # Generate random values within specified ranges
@@ -35,26 +52,26 @@ def generate_plant_data():
 
 
 
-@app.get('/chat/research')
+@app.get('/chat/research', response_model=JsonOutput)
 def chat_research(query: str = Query(...)):
     try:
         research_results = research_agent.run(
             message=query
         )
-        return {"message": research_results.content}
+        return JsonOutput(output=research_results.content)
     except:
-        return {"message": "not available right now"}
+        return JsonOutput(output="not available right now")
 
 
-@app.get('chat/rag')
+@app.get('chat/rag', response_model=JsonOutput)
 def chat_rag(query: str = Query(...)):
     try:
         rag_results = rag_agent.run(
             message=query
         )
-        return {"message": rag_results.content}
+        return JsonOutput(output=rag_results.content)
     except:
-        return {"message": "not available right now"}
+        return JsonOutput(output="not available right now")
 
 
 @app.get('/chat/enterprise/admin')
@@ -79,34 +96,22 @@ async def chat_ent_admin(background_task: BackgroundTasks):
         )
 
         background_task.add_task(fm.send_message, message, template_name="financial_report.html")
-        return {"message": f"Your financial report: {result}"}
+        return JsonOutput(output=f"Your financial report: {result}")
     except:
-        return {"message": "You have already reached the limit for your Quarterly financial analysis Generation, Contact Gsync customer service for extension."}
+        return JsonOutput(output="You have already reached the limit for your Quarterly financial analysis Generation, Contact Gsync customer service for extension.")
 
 
-@app.get('/chat/iot')
+@app.get('/chat/iot', response_model=JsonOutput)
 def chat_ent_user():
     iot_data = generate_plant_data()
     try:
         result = IotAnalystCrew().crew().kickoff(inputs={
             "plant_data": iot_data
         })
-        return {"message": result}
+        return JsonOutput(output=result)
     except:
-        return {"message": "iot analysis waiting in queue, sending via email."}
+        return JsonOutput(output="iot analysis waiting in queue, sending via email.")
 
-
-origins = [
-    'http://localhost:3000'
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*']
-)
 
 if __name__ == "__main__":
     status = uvicorn.run(app, host="0.0.0.0", port=88)
